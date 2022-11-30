6be286bf6228b9dc002d2a462bf91984d3115fba
Evan Bowman <ebowman@bu.edu>
2022-11-29 19:09:16 -0500

    add comments

--- send_multiboot.h
+++ send_multiboot.h
@@ -16,8 +16,8 @@ enum MbResult
 #define MB_DEFAULT_TIMEOUT 200000
 
 
-enum MbResult mb_send_rom(u16* begin,
-                          u16* end,
+enum MbResult mb_send_rom(u16* begin, // pointer to beginning of multiboot rom
+                          u16* end,   // pointer to end of multiboot rom (will be rounded up s.t. (end-begin) % 0x10 == 0)
                           int timeout);
 
 
